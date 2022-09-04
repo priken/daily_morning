fdbf2121cf0e817ed31a09c6cf5489aed4dad43e
kkk <1694286141@qq.com>
2022-09-04 21:11:08 +0800

    表情包接口

--- main.py
+++ main.py
@@ -45,10 +45,8 @@ def get_birthdayman():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+  words = "相逢的意义在于照亮彼此"
+  return words
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
