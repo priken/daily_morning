7f24463125dab56e80de3103450fde136b1f93b0
kkk <1694286141@qq.com>
2022-09-04 19:24:43 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from datetime import date, datetime
+import math
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage, WeChatTemplate
+import requests
+import os
+import random
+
+#服务器的repository secret
+today = datetime.now()
+start_date = os.environ['START_DATE']
+city = os.environ['CITY']
+birthdayman = os.environ['BIRTHDAYMAN']
+birthdaywoman = os.environ['BIRTHDAYWOMAN']
+
+app_id = os.environ["APP_ID"]
+app_secret = os.environ["APP_SECRET"]
+
+user_id = os.environ["USER_ID"]
+template_id = os.environ["TEMPLATE_ID"]
+
+#接口
+def get_weather():
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  res = requests.get(url).json()
+  weather = res['data']['list'][0]
+  return weather['weather'], math.floor(weather['temp'])
+
+def get_count():
+  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  return delta.days
+
+def get_birthdaywoman():
+  next = datetime.strptime(str(date.today().year) + "-" + birthdaywoman, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+def get_birthdayman():
+  next = datetime.strptime(str(date.today().year) + "-" + birthdayman, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+def get_words():
+  words = requests.get("https://api.shadiao.pro/chp")
+  if words.status_code != 200:
+    return get_words()
+  return words.json()['data']['text']
+
+def get_random_color():
+  return "#%06x" % random.randint(0, 0xFFFFFF)
+
+
+
+#输出内容
+client = WeChatClient(app_id, app_secret)
+
+wm = WeChatMessage(client)
+wea, temperature = get_weather()
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_leftwoman":{"value":get_birthdaywoman()},"birthday_leftman":{"value":get_birthdayman()},"words":{"value":get_words(), "color":get_random_color()}}
+res = wm.send_template(user_id, template_id, data)
+print(res)
